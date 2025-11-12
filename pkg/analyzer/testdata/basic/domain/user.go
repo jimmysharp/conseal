e8aa7af7bfb99fbeafad9987e4a4b72f34cd98f1
Ryosuke Yamada <jimmy.sharptech@gmail.com>
2025-11-13 01:25:21 +0900

    test: more practical domain example

--- pkg/analyzer/testdata/basic/domain/user.go
+++ pkg/analyzer/testdata/basic/domain/user.go
@@ -1,6 +1,6 @@
 package domain
 
-import "math/rand/v2"
+import "fmt"
 
 type User struct {
 	ID   int
@@ -8,18 +8,20 @@ type User struct {
 	Age  int
 }
 
-func NewUser(id int, name string, age int) *User {
+func NewUser(id int, name string, age int) (*User, error) {
+	if id <= 0 {
+		return nil, fmt.Errorf("id must be positive: %d", id)
+	}
+	if name == "" {
+		return nil, fmt.Errorf("name must not be empty")
+	}
+	if age < 0 {
+		return nil, fmt.Errorf("age must be non-negative: %d", age)
+	}
+
 	return &User{
 		ID:   id,
 		Name: name,
 		Age:  age,
-	}
-}
-
-func CreateUser(name string, age int) *User {
-	return &User{
-		ID:   rand.Int(),
-		Name: name,
-		Age:  age,
-	}
+	}, nil
 }
